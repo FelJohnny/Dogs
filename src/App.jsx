100dcb2d6468806454f48faa50a230748510e8f7
Felipe Johnny <felipijohnny@outlook.com>
2024-03-04 22:48:18 -0300

    aplicando primeiro commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './app.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+import Dog from '../../Assets/dogs.svg?react'
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label='Dogs - Home'>
+          <Dog/>
+        </Link>
+        <Link className={styles.login} to="/login">Login/ Criar Login</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
